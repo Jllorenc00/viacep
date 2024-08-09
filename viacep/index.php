0df18f926d2f258bc50ba6906779b694d1cb71a9
Joao Lorenco <jllorenc00@gmail.com>
2024-08-09 20:14:25 -0300

    first commit

---
+++ viacep/index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>VIA CEP</title>
+    <script src="script.js"></script>
+    <link rel="styleheet" hef="style.css">
+</head>
+<body>
+    <h1> VIA CEP </h1>
+    <form>
+
+        <input type="text" name="cep" id="cep" placeholder="CEP" onblur="buscaCep()">
+
+        <br>
+
+        <input type="text" name="logradouro" id="logradouro" placeholder="Logradouro">
+
+        <br>
+        
+        <input type="text" name="numero" id="numero" placeholder="Número">
+        
+        <br>
+        
+        <input type="text" name="complemento" id="complemento" placeholder="Complemento">
+        
+        <br>
+        
+        <input type="text" name="bairro" id="bairro" placeholder="Bairro">
+        
+        <br>
+        
+        <input type="text" name="uf" id="uf" placeholder="UF">
+        
+        <br>
+        
+        <input type="text" name="localidade" id="localidade" placeholder="Localidade">
+        
+        <br>
+        
+        <input type="text" name="ibge" id="ibge" placeholder="IBGE">
+        
+        <br>
+        
+        <input type="text" name="ddd" id="ddd" placeholder="DDD">
+        
+        <br>
+
+    </form>
+</body>
+</html>
